684f1bfa0360c3cb77e3d3833d2e2cfbc513d163
ahsanhf <ahsanhf@gmail.com>
2024-05-11 08:38:38 +0700

    Setel nilai data pada newNode

--- 0014_Stock.cpp
+++ 0014_Stock.cpp
@@ -26,3 +26,4 @@ Stack() {
 //Push operation: insert an element onto the top of the stack
 int push(int value) {
 	Node* newNode = new Node(); //1.Allocate memory forr the new node
+	newNode->data = value; //2.assign value
